59c8900ec3b1da5f788e80ab62bb48b2008d5e04
PalomoGonzalo <89616271+PalomoGonzalo@users.noreply.github.com>
2022-09-06 22:40:19 -0300

    controles html

--- Clase01/js/index.js
+++ Clase01/js/index.js
@@ -20,17 +20,6 @@ window.addEventListener("load",function ()
     });
 });
 
-
-*/
-
-
-window.onload=function ()
-{
-    document.getElementById("btnSaludar").addEventListener("click",function (){
-        alert("Hola bienvenido a js");
-    });
-};
-
 function sumar(a,b){
     return a + b;
 }
@@ -48,6 +37,32 @@ const sumarrr=(a,b)=>a+b;
 console.log(sumar(3,4));
 console.log(sumarr(3,4));
 console.log(sumarrr(3,4));
+*/
+
+
+window.onload=function ()
+{
+    document.getElementById("btnSaludar").addEventListener("click",function (e){
+        console.log("boton");
+        console.log(e.target)
+    });
+};
+
+document.getElementById("contenedor").addEventListener("click",function (e)
+{
+    console.log("contenedor");
+    e.stopPropagation();
+    console.log(e.target);
+},true);/// quiero que capture el evento en fase de captura, si es falso lo toma en default en burbuja
+
+
+const formularo=document.getElementsByTagName("form")[0];  //devuelve un array
+formularo.addEventListener("submit", function(e)
+{
+    //e.preventDefault();
+    console.log(e.target);
+});// eevento submit lo lanza el formulario
+
 
 
 
